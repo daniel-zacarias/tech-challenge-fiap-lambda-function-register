49051e2f43af719c1f871c85c8d9a0b57982f0f8
Daniel Zacarias <>
2025-12-03 22:49:41 -0300

    Add validate

--- src/main/java/fiap/dto/FeedbackRequest.java
+++ src/main/java/fiap/dto/FeedbackRequest.java
@@ -1,4 +1,14 @@
 package fiap.dto;
 
-public record FeedbackRequest(String descricao, int nota) {
+import io.smallrye.common.constraint.NotNull;
+
+public record FeedbackRequest(
+        @NotNull String descricao,
+        @NotNull int nota) {
+
+    public FeedbackRequest {
+        if (nota < 1 || nota > 10) {
+            throw new IllegalArgumentException("Nota must be between 1 and 5");
+        }
+    }
 }
