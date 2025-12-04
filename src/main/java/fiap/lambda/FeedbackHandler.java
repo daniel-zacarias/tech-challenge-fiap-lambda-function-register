b03f543cbada8c7c1055ecc8f6bd333116bf7622
Daniel Zacarias <>
2025-12-03 22:26:20 -0300

    Usando o APIGatewayV2HTTPEvent para obter o evento do Gateway

--- src/main/java/fiap/lambda/FeedbackHandler.java
+++ src/main/java/fiap/lambda/FeedbackHandler.java
@@ -2,6 +2,7 @@ package fiap.lambda;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import fiap.dto.FeedbackRequest;
@@ -13,16 +14,19 @@ import software.amazon.awssdk.services.sns.model.PublishRequest;
 import java.time.Instant;
 import java.util.UUID;
 
-public class FeedbackHandler implements RequestHandler<FeedbackRequest, FeedbackResponse> {
+public class FeedbackHandler implements RequestHandler<APIGatewayV2HTTPEvent, FeedbackResponse> {
     SnsClient snsClient = SnsClient.builder().build();
 
     @Override
-    public FeedbackResponse handleRequest(FeedbackRequest request, Context context) {
-        if (request == null) {
+    public FeedbackResponse handleRequest(APIGatewayV2HTTPEvent event, Context context) {
+        if (event == null) {
             context.getLogger().log("Received null request");
             FeedbackResponse r = new FeedbackResponse("ERROR", null, Instant.now().toString());
             return r;
         }
+        String body = event.getBody();
+        ObjectMapper mapper = new ObjectMapper();
+        FeedbackRequest request = mapper.convertValue(body, FeedbackRequest.class);
 
         String id = UUID.randomUUID().toString();
         context.getLogger().log("Received feedback with ID: " + id);
@@ -34,7 +38,6 @@ public class FeedbackHandler implements RequestHandler<FeedbackRequest, Feedback
                     .topicArn(System.getenv("SNS_TOPIC_ARN"))
                     .message(json)
                     .build();
-
             snsClient.publish(publishRequest);
         } catch (Exception e) {
             context.getLogger().log("Error publishing message to SNS: " + e.getMessage());
